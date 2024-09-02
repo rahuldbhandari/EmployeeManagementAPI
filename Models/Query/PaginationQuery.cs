841e99c431776735c686794f8824c821882f6f41
Rahul Deb Bhandari <to.rdbhandari@gmail.com>
2024-09-02 07:31:43 +0530

    WIP

--- Models/Query/PaginationQuery.cs
+++ Models/Query/PaginationQuery.cs
@@ -2,17 +2,19 @@
 {
     public class PaginationQuery
     {
-        public PaginationQuery() { 
+        public PaginationQuery()
+        {
             PageNumber = 1;
             PageSize = 10;
         }
 
-        public PaginationQuery(int pageNumber, int pageSize) {
+        public PaginationQuery(int pageNumber, int pageSize)
+        {
             PageNumber = pageNumber;
             PageSize = (pageSize > 100) ? 100 : pageSize;
         }
 
-        public int PageNumber { get; set; } 
+        public int PageNumber { get; set; }
         public int PageSize { get; set; }
     }
 }
